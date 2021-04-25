d92e3e817732a1a22b6ee7361b12fade87df9891
DouNai996 <80383844+DouNai996@users.noreply.github.com>
2021-04-26 00:36:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -25,18 +25,15 @@ if(is_array($_GET)&&count($_GET)>0)//判断是否有Get参数
 			while($row = $result->fetch_assoc()) {
 				$title=$row["title"];
 				$content=$row["content"];
-				$time = $row["time"];
+				$time = " 时间：".$row["time"];
 			}
 		} else {
 			$title="404";
 			$content="你所访问的网址可能有问题";
+			
 		}
 		$conn->close();
-		$txt=file_get_contents('mb.html');
-		$txt=str_replace('@@@@@',$title,$txt);
-		$txt=str_replace('#####',$content,$txt);
-		$txt=str_replace('$$$$$',$time,$txt);
-		//可以类似的语句替换许多变量
+		$txt="<html lang="."en"."><head><style>.container{padding:10px;box-sizing:border-box;width:100%;height:100%}.close{}.img-close{width:20px;height:20px}.title{font-size:22px;font-weight:400;color:#333;line-height:33px;margin:0px 0 10px 0}.date{font-size:15px;font-weight:500;color:#999;line-height:15px;margin-bottom:30px}.cont-wrap{white-space:normal;word-break:break-all;word-wrap:break-word;padding:0px;border-radius:10px;border:2px dashed#ccc}.cont{font-size:16px;font-weight:500;color:#666;line-height:30px;padding:10px}.span{color:#000}.label{background-color:rgb(45,183,245);color:#1890ff;background:#e6f7ff;border-color:#91d5ff;margin-right:8px;padding:0 7px}.messageType{color:#1890ff;background:#e6f7ff}.auth{font-size:15px;font-weight:bold;color:#000000}h4{margin:0px;padding:0px}hr{border-style:solid;border-width:1px 0px 0px;border-color:rgba(0,0,0,0.1);transform-origin:0px 0px;transform:scale(1,0.5)}.td{margin:15px 0 0 0;text-align:center;display:none}</style><title>".$title."</title></head><body><div class="."container"."><div class="."title".">".$title."</div><div class="."date"."><span class="."auth".">豆奶酱</span><span>".$time."</span></div><div class="."cont-wrap"."><div class="."cont".">".$content."</div></div></body></html>";
 		echo $txt;
 	}
 }
